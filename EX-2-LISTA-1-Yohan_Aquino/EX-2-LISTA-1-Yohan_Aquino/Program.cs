b8cee2a7f84bebf2e441fa4e26d3d656cb67b047
YohanAquino <74798062+YohanAquino@users.noreply.github.com>
2020-11-27 21:27:15 -0300

    Add files via upload

---
+++ EX-2-LISTA-1-Yohan_Aquino/EX-2-LISTA-1-Yohan_Aquino/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_2_LISTA_1_Yohan_Aquino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Calcular e exibir a área de um quadrado, a partir do valor de sua aresta que será digitado. 
+            Quadrado qua = new Quadrado();
+
+            Console.WriteLine("digite a aresta do quadrado:");
+            qua.setLado(double.Parse(Console.ReadLine()));
+            qua.calcularArea();
+            Console.WriteLine("A área do quadrado é {0}", qua.getArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EX-2-LISTA-1-Yohan_Aquino/EX-2-LISTA-1-Yohan_Aquino/Quadrado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_2_LISTA_1_Yohan_Aquino
+{
+    class Quadrado
+    {
+        private double l;
+        private double a;
+
+        public void setLado(double x)
+        {
+            l = x;
+        }
+
+        public void calcularArea()
+        {
+            a = Math.Pow(l, 2);
+        }
+        public double getArea()
+        {
+            return a;
+        }
+
+    }
+}
